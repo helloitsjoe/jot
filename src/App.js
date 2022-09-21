2d6c46b6496662cccb6e514de9e3f9dfcd9f8368
helloitsjoe <jboyle617@gmail.com>
2022-09-21 10:05:51 -0400

    fix form

--- src/App.js
+++ src/App.js
@@ -122,7 +122,7 @@ export default function App({ api, onSignOut }) {
 
   return (
     <Box maxWidth="500px" m="2em auto">
-      <Box onSubmit={handleAddNote} m="1em 0">
+      <Box as="form" onSubmit={handleAddNote} m="1em 0">
         <Input
           width="auto"
           label={<h3>Add a note</h3>}
