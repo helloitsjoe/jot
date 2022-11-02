ef756b6dac3ef49f535a372c2d19b969f48a8b5a
helloitsjoe <jboyle617@gmail.com>
2022-11-01 23:18:48 -0400

    skip tests

--- src/__tests__/Tag.test.js
+++ src/__tests__/Tag.test.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import {
-  fireEvent,
-  render,
-  screen,
-  waitForElementToBeRemoved,
-} from '@testing-library/react';
-import Tag from '../Tag';
+// import React from 'react';
+// import {
+//   fireEvent,
+//   render,
+//   screen,
+//   waitForElementToBeRemoved,
+// } from '@testing-library/react';
+// import Tag from '../Tag';
+
 // import RawNotes from '../components/Notes';
 // import { withSWR } from '../utils';
 // import ModalProvider from '../components/Modal';
 
 describe('Tag', () => {
-  it('tag name is editable', async () => {
-    render(<Tag />);
-  });
+  it.todo('tag name is editable');
 
   it.todo('tag color is editable');
 });
