ffb54b2c50890a11336a8361927490930bec6d39
helloitsjoe <jboyle617@gmail.com>
2023-03-29 21:53:41 -0400

    tests

--- src/__tests__/Tag.test.js
+++ src/__tests__/Tag.test.js
@@ -1,18 +1,30 @@
-// import React from 'react';
-// import {
-//   fireEvent,
-//   render,
-//   screen,
-//   waitForElementToBeRemoved,
-// } from '@testing-library/react';
-// import Tag from '../Tag';
-
-// import RawNotes from '../components/Notes';
-// import { withSWR } from '../utils';
-// import ModalProvider from '../components/Modal';
+import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import Tag from '../components/Tag';
 
 describe('Tag', () => {
   it.todo('tag name is editable');
 
   it.todo('tag color is editable');
+
+  it('onSelect is called when text is pressed', () => {
+    const onSelect = jest.fn();
+    render(<Tag onSelect={onSelect}>Foo</Tag>);
+    expect(onSelect).toBeCalledTimes(0);
+    fireEvent.click(screen.queryByText('Foo'));
+    expect(onSelect).toBeCalledTimes(1);
+  });
+
+  it('onDelete is called when X is pressed', () => {
+    const onDelete = jest.fn();
+    render(<Tag onDelete={onDelete}>Foo</Tag>);
+    expect(onDelete).toBeCalledTimes(0);
+    fireEvent.click(screen.queryByText('X'));
+    expect(onDelete).toBeCalledTimes(1);
+  });
+
+  it('X is hidden if onDelete is absent', () => {
+    render(<Tag>Foo</Tag>);
+    expect(screen.queryByText('X')).not.toBeTruthy();
+  });
 });
